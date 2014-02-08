e9a0570cdcd932e98f09d2c016dd00d5a46bd76c
Jhaliya <praveen.nitdgp@gmail.com>
2014-02-08 21:06:45 +0530

    left/right gesture can be used to reveal the left/right/center controllers

--- JHRootViewController.m
+++ JHRootViewController.m
@@ -10,6 +10,7 @@
 #import "JHLeftViewController.h"
 #import "JHCenterViewController.h"
 #import "JHRightViewController.h"
+#import "JHConstant.h"
 
 @implementation JHRootViewController {
   JHLeftViewController *leftController;
@@ -17,135 +18,131 @@
   JHRightViewController *rightController;
 
   UIView *currentView;
+  CGPoint lastPoint;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
   self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
   if (self) {
     // Custom initialization
+    self.view.autoresizesSubviews = YES;
+    self.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    
+    leftController = [[JHLeftViewController alloc] initWithNibName:nil bundle:nil];
+    centerController = [[JHCenterViewController alloc] initWithNibName:nil bundle:nil];
+    rightController = [[JHRightViewController alloc] initWithNibName:nil bundle:nil];
+ 
+    [self.view addSubview:leftController.view];
+    [self.view addSubview:centerController.view];
+    [self.view addSubview:rightController.view];
+    
+    currentView = centerController.view;
   }
   return self;
 }
 
 -(void)viewWillAppear:(BOOL)animated  {
   [super viewWillAppear:animated];
+  
   centerController.view.frame = self.view.frame;
-  leftController.view.frame =  CGRectMake(-320, 0, self.view.frame.size.width, self.view.frame.size.height);
-  rightController.view.frame =  CGRectMake(320, 0, self.view.frame.size.width, self.view.frame.size.height);
+  leftController.view.frame =  CGRectMake(-KSCREEN_WIDTH, 0, self.view.frame.size.width,
+                                          self.view.frame.size.height);
+  rightController.view.frame =  CGRectMake(KSCREEN_WIDTH, 0, self.view.frame.size.width,
+                                           self.view.frame.size.height);
+  
+  UISwipeGestureRecognizer * swipeleft=[[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeleft:)];
+  swipeleft.direction=UISwipeGestureRecognizerDirectionLeft;
+  [self.view addGestureRecognizer:swipeleft];
+
+  UISwipeGestureRecognizer * swiperight=[[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swiperight:)];
+  swiperight.direction=UISwipeGestureRecognizerDirectionRight;
+  [self.view addGestureRecognizer:swiperight];
+  
+}
+
+-(void)swipeleft:(UISwipeGestureRecognizer*)gestureRecognizer {
+  if(currentView == centerController.view) {
+    [self rightRootButtonBarEvent:nil];
+  } else if(currentView == leftController.view) {
+    [self leftRootButtonBarEvent:nil];
+  }
+  
+  NSLog(@"Left swipe gesture");
+}
+
+-(void)swiperight:(UISwipeGestureRecognizer*)gestureRecognizer {
+  NSLog(@"Right swipe gesture");
+  
+  if(currentView == centerController.view) {
+    [self leftRootButtonBarEvent:nil];
+  } else if(currentView == rightController.view) {
+    [self rightRootButtonBarEvent:nil];
+  }
+  
 }
 
 -(void) loadView {
   [super loadView];
   
-  leftController = [[JHLeftViewController alloc] initWithNibName:nil bundle:nil];
-  centerController = [[JHCenterViewController alloc] initWithNibName:nil bundle:nil];
-  rightController = [[JHRightViewController alloc] initWithNibName:nil bundle:nil];
-  
   self.title = @"Root controller";
-  self.view.backgroundColor = [UIColor greenColor];
-  
   self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(leftRootButtonBarEvent:)];
-  
   self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(rightRootButtonBarEvent:)];
-  
-  [self.view addSubview:leftController.view];
-  [self.view addSubview:centerController.view];
-  [self.view addSubview:rightController.view];
-  
-  currentView = centerController.view;
 }
 
 -(void) rightRootButtonBarEvent:(id)sender {
-  
-  NSLog(@"right button clicked");
   
   CGRect rRect;
   CGRect centerRect;
   
   if(currentView == centerController.view) {
     currentView = rightController.view;
-    rRect = CGRectMake(50, 0, self.view.frame.size.width - 50, self.view.frame.size.height);
-    centerRect = CGRectMake(-320+50, 0, self.view.frame.size.width - 50, self.view.frame.size.height);
-    NSLog(@"Current view is center view");
+    rRect = CGRectMake(KREVEAL_GAP, 0, self.view.frame.size.width,
+                       self.view.frame.size.height);
+    centerRect = CGRectMake(-KSCREEN_WIDTH + KREVEAL_GAP, 0, self.view.frame.size.width,
+                            self.view.frame.size.height);
   } else {
     currentView = centerController.view;
-    rRect = CGRectMake(320, 0, self.view.frame.size.width, self.view.frame.size.height);
+    rRect = CGRectMake(KSCREEN_WIDTH, 0, self.view.frame.size.width, self.view.frame.size.height);
     centerRect  = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-    NSLog(@"Current view is left view");
   }
-  
-  
-  NSLog(@"Rigth view moving to %@",NSStringFromCGRect(rRect));
-  NSLog(@"center view moving from %@",NSStringFromCGRect(centerRect));
-  /*
-  [UIView animateWithDuration:2.0 delay:0.0
-                      options:UIViewAnimationOptionCurveEaseInOut animations:^{
-                        rightController.view.frame = rRect ;
-                        centerController.view.frame = centerRect;
-                        self.navigationController.navigationBar.frame = CGRectMake(centerRect.origin.x, 20, 320, 44);
-                      } completion:^(BOOL finished) {
-                        
-                      }];
-   
-   */
   
   [UIView animateWithDuration:1.0 delay:0.0 usingSpringWithDamping:1.0 initialSpringVelocity:0.1
                       options:UIViewAnimationOptionCurveEaseInOut animations:^{
                         rightController.view.frame = rRect ;
                         centerController.view.frame = centerRect;
-                        self.navigationController.navigationBar.frame = CGRectMake(centerRect.origin.x, 20, 320, 44);
+                        self.navigationController.navigationBar.frame = CGRectMake(centerRect.origin.x, 20, KSCREEN_WIDTH, 44);
                       } completion:^(BOOL finished) {
                         
                       }];
 }
 
 -(void) leftRootButtonBarEvent:(id)sender {
-  NSLog(@"Left button clicked");
   
   CGRect leftRect;
   CGRect centerRect;
   
   if(currentView == centerController.view) {
     currentView = leftController.view;
-    leftRect = CGRectMake(0, 0, self.view.frame.size.width - 50, self.view.frame.size.height);
-    centerRect = CGRectMake(320 - 50, 0, self.view.frame.size.width - 50, self.view.frame.size.height);
-    NSLog(@"Current view is center view");
+    leftRect = CGRectMake(0, 0, self.view.frame.size.width - KREVEAL_GAP, self.view.frame.size.height);
+    centerRect = CGRectMake(KSCREEN_WIDTH - KREVEAL_GAP, 0, self.view.frame.size.width - KREVEAL_GAP,
+                            self.view.frame.size.height);
   } else {
-    
     currentView = centerController.view;
-    leftRect = CGRectMake(-320, 0, self.view.frame.size.width, self.view.frame.size.height);
+    leftRect = CGRectMake(-KSCREEN_WIDTH, 0, self.view.frame.size.width, self.view.frame.size.height);
     centerRect  = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-    NSLog(@"Current view is left view");
   }
-  
-  
-  NSLog(@"Left view moving to %@",NSStringFromCGRect(leftRect));
-  NSLog(@"center view moving from %@",NSStringFromCGRect(centerRect));
-  
-  /*
-  [UIView animateWithDuration:2.0 delay:0.0
-                      options:UIViewAnimationOptionCurveEaseInOut animations:^{
-                        
-                        leftController.view.frame = leftRect ;
-                        centerController.view.frame = centerRect;
-                        self.navigationController.navigationBar.frame = CGRectMake(centerRect.origin.x, 20, 320, 44);
-                        
-                      } completion:^(BOOL finished) {
-                        
-                        
-                      }];
-   */
   
   [UIView animateWithDuration:1.0 delay:0.0 usingSpringWithDamping:1.0 initialSpringVelocity:0.1
                       options:UIViewAnimationOptionCurveEaseInOut animations:^{
                         leftController.view.frame = leftRect ;
                         centerController.view.frame = centerRect;
-                        self.navigationController.navigationBar.frame = CGRectMake(centerRect.origin.x, 20, 320, 44);
+                        self.navigationController.navigationBar.frame = CGRectMake(centerRect.origin.x, 20, KSCREEN_WIDTH, 44);
                       } completion:^(BOOL finished) {
                         
                       }];
   
 }
+
 - (void)viewDidLoad {
   [super viewDidLoad];
 	// Do any additional setup after loading the view.
