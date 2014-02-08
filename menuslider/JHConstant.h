4315b51255e6742701541d19071805232e3f0c27
Jhaliya <praveen.nitdgp@gmail.com>
2014-02-08 21:05:34 +0530

    constant file added

---
+++ menuslider/JHConstant.h
@@ -0,0 +1,16 @@
+//
+//  JHConstant.h
+//  JHMenuSlider
+//
+//  Created by Praveen Sharma on 08/02/14.
+//  Copyright (c) 2014 Jhaliya. All rights reserved.
+//
+
+#ifndef JHMenuSlider_JHConstant_h
+#define JHMenuSlider_JHConstant_h
+
+#define KREVEAL_GAP 75.0f
+#define KSCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+
+
+#endif
